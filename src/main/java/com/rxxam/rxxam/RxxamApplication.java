3555fe8fe8ad467a193fa44727bfbcd0ade38c43
rayeesGmail <shaikhrayeesahmed@gmail.com>
2018-11-20 19:38:39 +0530

    add spring web, hataos, jpa, mysql, h2.

---
+++ src/main/java/com/rxxam/rxxam/RxxamApplication.java
@@ -0,0 +1,12 @@
+package com.rxxam.rxxam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxxamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RxxamApplication.class, args);
+    }
+}
---
+++ src/test/java/com/rxxam/rxxam/RxxamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rxxam.rxxam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RxxamApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
